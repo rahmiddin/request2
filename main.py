62dccca000bdd0b44c01e02f5d0d5e459d248416
Rahmidin Hasanov <rahmiddin.hasanov@bk.ru>
2022-05-05 23:40:59 +0500

    fulls

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pprint import pprint
 TOKEN = '2619421814940190'
 URL = 'https://superheroapi.com/api/2619421814940190/search/'
 
+
 def power_stats(url):
     heroes_name = ['Hulk', 'Captain America', 'Thanos']
     heroes_list = {}
@@ -12,10 +13,12 @@ def power_stats(url):
         response = requests.get(url + name).json()
         heroes_list[name] = int(response['results'][0]['powerstats']['intelligence'])
     return heroes_list
-def all_info(url):
-    response = requests.get(url + 'Hulk')
-    return response.headers
+
+
+def strongrer(url):
+    heroes_stats = power_stats(url)
+    return (max(heroes_stats, key=heroes_stats.get))
+
 
 if __name__ == '__main__':
-    print(power_stats(URL))
-    pprint(all_info(URL))
+    print(strongrer(URL))
